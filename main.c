a3f2d6786d92a1d65ceca460d2e4da73fa1f79c1
Emc54 <35582806+Emc54@users.noreply.github.com>
2025-01-21 21:39:15 +0000

    Ready. Set. Go!

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+
+void mell_loop(){
+}
+
+int main(int argc, char **argv)
+{
+
+    //load config files if any
+
+    //main loop
+    mell_loop();    
+    
+    //exit
+    return EXIT_SUCCESS;
+
+}
